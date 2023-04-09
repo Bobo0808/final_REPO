5179e34e39d2e41fcab94d610373c767d2cadedf
frank1209 <frank1209@gmail.com>
2023-04-09 23:26:41 +0800

    addmap

---
+++ MapResearch/Map/atlas_32x1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="atlas_32x" tilewidth="32" tileheight="32" tilecount="512" columns="32">
+ <image source="../img/Package_Marketplace/atlas_32x.png" width="1024" height="512"/>
+</tileset>
---
+++ MapResearch/Map/js/font.js
@@ -0,0 +1,132 @@
+class Sprite {
+    constructor({
+        position,
+        velocity,
+        image,
+        frames = { max: 1, hold: 10 },
+        sprites,
+        animate = false,
+        rotation = 0,
+        scale = 1
+    }) {
+        this.position = position
+        this.image = new Image()
+        this.frames = { ...frames, val: 0, elapsed: 0 }
+        this.image.onload = () => {
+            this.width = (this.image.width / this.frames.max) * scale
+            this.height = this.image.height * scale
+        }
+        this.image.src = image.src
+
+        this.animate = animate
+        this.sprites = sprites
+        this.opacity = 1
+
+        this.rotation = rotation
+        this.scale = scale
+    }
+
+    draw() {
+        ctx.save()
+        ctx.translate(
+            this.position.x + this.width / 2,
+            this.position.y + this.height / 2
+        )
+        ctx.rotate(this.rotation)
+        ctx.translate(
+            -this.position.x - this.width / 2,
+            -this.position.y - this.height / 2
+        )
+        ctx.globalAlpha = this.opacity
+
+        const crop = {
+            position: {
+                x: this.frames.val * (this.width / this.scale),
+                y: 0
+            },
+            width: this.image.width / this.frames.max,
+            height: this.image.height
+        }
+
+        const image = {
+            position: {
+                x: this.position.x,
+                y: this.position.y
+            },
+            width: this.image.width / this.frames.max,
+            height: this.image.height
+        }
+
+        ctx.drawImage(
+            this.image,
+            crop.position.x,
+            crop.position.y,
+            crop.width,
+            crop.height,
+            image.position.x,
+            image.position.y,
+            image.width * this.scale,
+            image.height * this.scale
+        )
+
+        ctx.restore()
+
+        if (!this.animate) return
+
+        if (this.frames.max > 1) {
+            this.frames.elapsed++
+        }
+
+        if (this.frames.elapsed % this.frames.hold === 0) {
+            if (this.frames.val < this.frames.max - 1) this.frames.val++
+            else this.frames.val = 0
+        }
+    }
+}
+
+
+class Boundary {
+    static width = 64;
+    static height = 64;
+    constructor({ position }) {
+        //位置相對做標定義
+        this.position = position
+        //地圖放大200%
+        this.width = 64
+        this.height = 64
+    }
+
+    draw() {
+        // ctx.fillStyle = 'rgba(255, 0, 0, 0)'
+        ctx.fillStyle = 'red'
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+class Character extends Sprite {
+    constructor({
+        position,
+        velocity,
+        image,
+        frames = { max: 1, hold: 10 },
+        sprites,
+        animate = false,
+        rotation = 0,
+        scale = 1,
+        dialogue = ['']
+    }) {
+        super({
+            position,
+            velocity,
+            image,
+            frames,
+            sprites,
+            animate,
+            rotation,
+            scale
+        })
+
+        this.dialogue = dialogue
+        this.dialogueIndex = 0
+    }
+}
