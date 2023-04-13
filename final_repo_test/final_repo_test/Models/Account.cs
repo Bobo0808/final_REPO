c3d847853dd54ee9b2b90e66c636a8012503aea1
Bobo0808 <wang32625171@gmail.com>
2023-04-13 16:27:15 +0800

    Models

---
+++ final_repo_test/final_repo_test/Models/Account.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace final_repo_test.Models
+{
+    public class Account
+    {
+        [Key]
+        public int A_ID { get; set; }
+        public string A_Name { get; set; }
+        public string UserName { get; set; }
+        public string UserPWD { get; set; }
+        public int? A_Gender { get; set; }
+        public DateTime? Birthday { get; set; }
+        public int? A_level { get; set; }
+        public string A_Email { get; set; }
+        public string? A_Phone { get; set; }
+        public string? A_add { get; set; }
+        public DateTime A_RegisteredAt { get; set; }
+    }
+}
---
+++ final_repo_test/final_repo_test/Models/LoginStaus.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace final_repo_test.Models
+{
+    public class LoginStaus
+    {
+        [Key]
+        public int L_ID { get; set; }
+        public int? A_ID { get; set; }
+        public DateTime L_cTime { get; set; }
+        public DateTime L_dcTime { get; set; }
+    }
+}
