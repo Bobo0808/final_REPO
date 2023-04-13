653a3b9f58cd79662265a2d9e2eeb5f86413720a
Bobo0808 <wang32625171@gmail.com>
2023-04-14 01:22:11 +0800

    Fkkey not finish

--- final_repo_test/final_repo_test/Models/Debuglog.cs
+++ final_repo_test/final_repo_test/Models/Debuglog.cs
@@ -8,7 +8,7 @@ namespace final_repo_test.Models
         public int D_ID { get; set; }
         public int A_ID { get; set; }
         public DateTime D_time { get; set; }
-        public string D_event { get; set; }
+        public string? D_event { get; set; }
         public bool D_isSolved { get; set; }
 
 
--- final_repo_test/final_repo_test/Models/Order.cs
+++ final_repo_test/final_repo_test/Models/Order.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace final_repo_test.Models
 {
@@ -6,6 +7,7 @@ namespace final_repo_test.Models
     {
         [Key]
         public int O_ID { get; set; }
+        [ForeignKey("Account")]
         public int O_A_ID { get; set; }
         public DateTime? O_Date { get; set; }
         public int? O_TotalPrice { get; set; }
