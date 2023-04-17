23faca803af10ec36857beca2ff9653614375cc8
z7016187 <asd880810@gmail.com>
2023-04-17 13:27:18 +0800

    20230417 bnn update db

--- final_repo_test/final_repo_test/Models/Account.cs
+++ final_repo_test/final_repo_test/Models/Account.cs
@@ -14,6 +14,9 @@ namespace final_repo_test.Models
         public Gender A_Gender { get; set; } = default!;
         [Column(TypeName = "Date")]
         public DateTime Birthday { get; set; } = default!;
+        [ForeignKey("Product")]
+        public int P_id { get; set; } = default!;
+        public Product Product { get; set; } = default!;
         public int A_level { get; set; } = default!;
         public string A_Email { get; set; } = default!;
         public string A_Phone { get; set; } = default!;
