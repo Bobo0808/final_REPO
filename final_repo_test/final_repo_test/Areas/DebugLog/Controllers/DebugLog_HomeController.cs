2a7f37d962fae0330abab69078594ff12e0b8e91
ashtar <ashtarloc@gmail.com>
2023-04-20 05:25:38 +0800

    ashtar: try to add AD's two pages's CRUD.

--- final_repo_test/final_repo_test/Areas/DebugLog/Controllers/DebugLog_HomeController.cs
+++ final_repo_test/final_repo_test/Areas/DebugLog/Controllers/DebugLog_HomeController.cs
@@ -1,36 +1,13 @@
-﻿using final_repo_test.Interfaces;
-using final_repo_test.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
-namespace final_repo_test.Areas.DebugLogs.Controllers
+namespace final_repo_test.Areas.DebugLog.Controllers
 {
     [Area("DebugLog")]
     public class DebugLog_HomeController : Controller
     {
-        private readonly IDebugLogRepository _debugLogRepository;
-        public DebugLog_HomeController(IDebugLogRepository debugLogRepository)
+        public IActionResult Index()
         {
-            _debugLogRepository = debugLogRepository;
-        }
-
-        public async Task<IActionResult> Index()
-        {
-            IEnumerable<DebugLog> debugLogs = await _debugLogRepository.GetAll();
-            return View(debugLogs);
-        }
-
-        public async Task<IActionResult> Detail(int id)
-        {
-            Console.WriteLine(id);
-            DebugLog debugLog = await _debugLogRepository.GetIdAsync(id);
-            return View(debugLog);
-        }
-
-        public async Task<IActionResult> Edit(int id)
-        {
-            DebugLog debugLogs = await _debugLogRepository.Edit(id);
-            _debugLogRepository.Update(debugLogs);
-            return RedirectToAction("Index");
+            return View();
         }
     }
 }
