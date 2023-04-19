a7c6fccdb04d0132999440b896a5a7a21f9b70c3
柏翔 <96458633+Bobo0808@users.noreply.github.com>
2023-04-19 15:02:54 +0800

    test

--- final_repo_test/final_repo_test/Areas/Login/Controllers/Login_HomeController.cs
+++ final_repo_test/final_repo_test/Areas/Login/Controllers/Login_HomeController.cs
@@ -4,10 +4,9 @@ namespace final_repo_test.Areas.Login.Controllers
 {
     public class Login_HomeController : Controller
     {
-        [Area("Login")]
         public IActionResult Index()
         {
-            return View();
+            return View("~/Areas/Login/Views/Login_Home/Index.cshtml");
         }
     }
 }
