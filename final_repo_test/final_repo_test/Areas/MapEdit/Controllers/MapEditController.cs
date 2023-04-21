53a6a05ee8217458db378feb755525e49fa439c6
frank <frank120910@gmail.com>
2023-04-21 14:57:26 +0800

    frank0421addmapedit

---
+++ final_repo_test/final_repo_test/Areas/MapEdit/Controllers/MapEditController.cs
@@ -0,0 +1,21 @@
+﻿using final_repo_test.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace final_repo_test.Areas.MapEdit.Controllers
+{
+    [Area(areaName: "MapEdit")]
+    public class MapEditController : Controller
+    {
+        
+
+        public readonly ApplicationDbContext _context;
+        public MapEditController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
