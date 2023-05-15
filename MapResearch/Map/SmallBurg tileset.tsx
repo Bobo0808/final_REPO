1ca192a642846255246285010c11664fa9ea9c77
frank1209 <frank1209@gmail.com>
2023-05-15 19:21:02 +0800

    0515frank update map

---
+++ MapResearch/Map/SmallBurg tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="SmallBurg tileset" tilewidth="32" tileheight="32" tilecount="368" columns="16">
+ <image source="../img/SmallBurg_village_pack_v1.6/Tileset/SmallBurg tileset.png" width="512" height="750"/>
+</tileset>
