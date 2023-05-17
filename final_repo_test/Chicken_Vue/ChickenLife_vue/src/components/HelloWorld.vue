a4332c7f6ac1ae3c0f0d240ffa82706800921ab9
z7016187 <asd880810@gmail.com>
2023-05-17 18:01:11 +0800

    bnn update vue

--- final_repo_test/Chicken_Vue/ChickenLife_vue/src/components/HelloWorld.vue
+++ final_repo_test/Chicken_Vue/ChickenLife_vue/src/components/HelloWorld.vue
@@ -18,7 +18,7 @@ const count = ref(0)
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
-
+  <p>{{Uid}}</p>
   <p>
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
