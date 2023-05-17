97e6ef06d8767343647fde2971ec0e6a6dd0f29c
z7016187 <asd880810@gmail.com>
2023-05-18 00:17:35 +0800

    20230518bnn vue

--- final_repo_test/Chicken_Vue/ChickenLife_vue/src/main.js
+++ final_repo_test/Chicken_Vue/ChickenLife_vue/src/main.js
@@ -1,7 +1,12 @@
-import { createApp } from 'vue'
+import { createApp, ref } from 'vue'
 import './style.css'
+
 import App from './App.vue'
 
 const app = createApp(App);
-app.config.globalProperties.Uid = '78';
+app.config.globalProperties.playerRef = ref({
+    id: 1,
+    name: ['shit', 'fuck', 'bitch', 'suck', 'cock'],
+    gender: 1
+});
 app.mount('#app')
