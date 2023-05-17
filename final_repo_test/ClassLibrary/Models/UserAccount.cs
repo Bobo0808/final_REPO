ae5f7c9712c8f91104fe496cbda3665a2d7ad03c
柏翔 <96458633+Bobo0808@users.noreply.github.com>
2023-05-17 16:39:45 +0800

    Account

--- final_repo_test/ClassLibrary/Models/UserAccount.cs
+++ final_repo_test/ClassLibrary/Models/UserAccount.cs
@@ -8,23 +8,23 @@ namespace ClassLibrary.Models
     {
         [Key]
         public int A_ID { get; set; } = default!;
-        public string A_Name { get; set; } = default!;
-        public string UserName { get; set; } = default!;
-        public string UserPWD { get; set; } = default!;
+        public string A_Name { get; set; } = "admin";
+        public string UserName { get; set; } = "admin";
+        public string UserPWD { get; set; } = "admin";
         public Gender A_Gender { get; set; } = default!;
         [Column(TypeName = "Date")]
-        public DateTime Birthday { get; set; } = default!;
+        public DateTime Birthday { get; set; }
         [ForeignKey("Product")]
-        public int P_id { get; set; } = default!;
+        public int P_id { get; set; } = 1;
         public Product Product { get; set; } = default!;
-        public int A_level { get; set; } = default!;
+        public int A_level { get; set; } =0;
         public string A_Email { get; set; } = default!;
-        public string A_Phone { get; set; } = default!;
-        public string A_add { get; set; } = default!;
+        public string? A_Phone { get; set; } = "";
+        public string? A_add { get; set; }= "";
         [Column(TypeName = "Date")]
         public DateTime A_RegisteredAt { get; set; } =DateTime.Now;
-        public string A_NickName { get; set; } = default!;
-        public int A_Coin { get; set; } = default!;
+        public string? A_NickName { get; set; } = "";
+        public int A_Coin { get; set; } = 300;
 
 
         public string? VerifycationToken { get; set; }
