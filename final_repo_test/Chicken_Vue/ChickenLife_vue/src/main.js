ea3da2771907be6db569b9443a6aa3afa6550eb7
z7016187 <asd880810@gmail.com>
2023-05-20 22:02:17 +0800

    bnn updated axios function

--- final_repo_test/Chicken_Vue/ChickenLife_vue/src/main.js
+++ final_repo_test/Chicken_Vue/ChickenLife_vue/src/main.js
@@ -2,30 +2,40 @@ import { createApp, ref } from 'vue'
 import './style.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import App from './AppSample.vue'
+import { response } from 'express';
+
+const baseAddress = "https://localhost:7093";
 
 const app = createApp(App);
 app.config.globalProperties.playerRef = ref({
-    id: 1,
-    name: ['shit', 'fuck', 'bitch', 'suck', 'cock'],
-    gender: 1
+  id: 1,
+  name: ['shit', 'fuck', 'bitch', 'suck', 'cock'],
+  gender: 1
 });
 
 export const fuck = () => {
-    alert('hello world');
-    return app.config.globalProperties;
-  };
-export const getAxios =(baseAddress,route,data)=>{
-    axios.get(baseAddress+route).then(response => {
-        data.value = response.data;
+  alert('hello world');
+  return app.config.globalProperties;
+};
+export const getAxios = (route, data) => {
+  axios.get(baseAddress + route).then(response => {
+    data.value = response.data;
 
-    });
-    return data.value
-  }
-export const postAxios =(baseAddress,route,data)=>{
-    axios.post(baseAddress+route).then(response => {
-        data.value = response.data;
-    });
-    return data.value
-  }
+  });
+  return data
+}
+export const postAxios = (route, data) => {
+  axios.post(baseAddress + route).then(response => {
+    data.value = response.data;
+  });
+  return data
+}
+
+export const putAxios = (route, data) => {
+  axios.put(baseAddress + route).then(response => {
+    data.value = response.data;
+  });
+  return data;
+}
 app.provide
 app.mount('#app')
