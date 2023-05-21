a13643cb9644abe32d87444ea6168700c32fa599
Bobo0808 <wang32625171@gmail.com>
2023-05-21 10:21:58 +0800

    cool vue by bobo

--- final_repo_test/Chicken_Vue/ChickenLife_vue/src/main.js
+++ final_repo_test/Chicken_Vue/ChickenLife_vue/src/main.js
@@ -5,27 +5,37 @@ import App from './AppSample.vue'
 
 const app = createApp(App);
 app.config.globalProperties.playerRef = ref({
-    id: 1,
-    name: ['shit', 'fuck', 'bitch', 'suck', 'cock'],
-    gender: 1
+  id: 1,
+  name: ['shit', 'fuck', 'bitch', 'suck', 'cock'],
+  gender: 1
 });
 
 export const fuck = () => {
-    alert('hello world');
-    return app.config.globalProperties;
-  };
-export const getAxios =(baseAddress,route,data)=>{
-    axios.get(baseAddress+route).then(response => {
-        data.value = response.data;
+  alert('hello world');
+  return app.config.globalProperties;
+};
+export const getAxios = (baseAddress, route, data) => {
+  axios.get(baseAddress + route).then(response => {
+    data.value = response.data;
 
-    });
-    return data.value
-  }
-export const postAxios =(baseAddress,route,data)=>{
-    axios.post(baseAddress+route).then(response => {
-        data.value = response.data;
-    });
-    return data.value
-  }
+  });
+  return data.value
+}
+export const postAxios = (baseAddress, route, data) => {
+  axios.post(baseAddress + route).then(response => {
+    data.value = response.data;
+  });
+  return data.value
+}
+const script1 = document.createElement('script');
+script1.src = 'https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js';
+script1.type = 'module';
+
+const script2 = document.createElement('script');
+script2.src = 'https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js';
+script2.setAttribute('nomodule', '');
+
+document.head.appendChild(script1);
+document.head.appendChild(script2);
 app.provide
 app.mount('#app')
